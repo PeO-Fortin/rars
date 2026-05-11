c9cb352165421f63e89a4e3f8eb7e02e27515464
Pierre-Olivier Fortin <fortin.pierre-olivier.2@courrier.uqam.ca>
2026-05-11 16:16:30 -0400

    Enléve les tests sans main

--- test/concolic/test.s
+++ test/concolic/test.s
@@ -1,17 +1,21 @@
 .data
 
+.eqv exit 10
+.eqv printInt 1
+
 mot: .word 42
 
 .text
 
-li t0, 55555555555
-li t1, 22222222222
-#li t2, 0x10000000000002
-mulw t3, t0, t1
+li t0, 43
 
-#la t1, mot
-#lw t0, 0(t1)
-#sw t0, 0(t1)
+la t1, mot
+sw t0, 0(t1)
+lw a0, 0(t1)
 
-li a7, 10
+
+li a7, printInt
+ecall
+
+li a7, exit
 ecall
